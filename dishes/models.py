6b70d79d18f8a3077059757ebb03e536524418fa
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-29 17:02:47 +0100

    Added DishType modell

--- dishes/models.py
+++ dishes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
