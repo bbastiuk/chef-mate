890cda29085ee088211c0e3bff4bfe7793686cc8
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-29 17:53:13 +0100

    Add Dish model with fields and relationships

--- dishes/models.py
+++ dishes/models.py
@@ -30,3 +30,12 @@ class Cook(AbstractUser):
         return f"{self.first_name} {self.last_name} ({experience_str})"
 
 
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey("DishType", on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField("Cook", related_name="dishes")
+
+    def __str__(self):
+        return f"{self.name} - {self.price}$"
