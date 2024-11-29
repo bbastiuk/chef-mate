81e1ae49bdb130415365b2cf88d8ab748e0ee15c
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-29 17:48:42 +0100

    Add Cook model with experience field

--- dishes/models.py
+++ dishes/models.py
@@ -13,3 +13,20 @@ class DishType(models.Model):
         return f"{self.name}"
 
 
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        if self.years_of_experience == 1:
+            experience_str = "1 year of experience"
+        elif 2 <= self.years_of_experience <= 4:
+            experience_str = f"{self.years_of_experience} years of experience"
+        else:
+            experience_str = f"{self.years_of_experience} years of experience"
+        return f"{self.first_name} {self.last_name} ({experience_str})"
+
+
