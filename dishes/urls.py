aaeab0cb50247dbcc36cf4daf9329cd129588787
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-12-02 17:13:38 +0100

    Add sidebar and pagination partial templates

--- dishes/urls.py
+++ dishes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'dishes'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dish-types/', views.DishTypeListView.as_view(), name='dish-type-list'),
+    path('dishes/', views.DishListView.as_view(), name='dish-list'),
+    path('cooks/', views.CookListView.as_view(), name='cook-list'),
 ]
--- dishes/views.py
+++ dishes/views.py
@@ -10,21 +10,14 @@ from .forms import DishForm, CookCreationForm, CookLicenseUpdateForm, CookSearch
 
 @login_required
 def index(request):
-    num_dishes = Dish.objects.count()
-    num_cooks = Cook.objects.count()
-    num_dish_types = DishType.objects.count()
-
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
     context = {
-        "num_dishes": num_dishes,
-        "num_cooks": num_cooks,
-        "num_dish_types": num_dish_types,
-        "num_visits": num_visits + 1,
+        'num_dishes': Dish.objects.count(),
+        'num_cooks': Cook.objects.count(),
+        'num_dish_types': DishType.objects.count(),
+        'num_visits': request.session.get('num_visits', 0),
     }
-
-    return render(request, "dishes/index.html", context=context)
+    request.session['num_visits'] = context['num_visits'] + 1
+    return render(request, 'dishes/index.html', context)
 
 
 class DishTypeListView(LoginRequiredMixin, generic.ListView):
