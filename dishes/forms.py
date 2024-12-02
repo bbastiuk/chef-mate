7dafd6fcce2eff6cdd9c38d3504df94972cd9948
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-12-02 16:49:56 +0100

    Add views for Dish, DishType, and Cook with corresponding forms

---
+++ dishes/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from .models import Dish, Cook, DishType
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Dish
+        fields = ["name", "description", "price", "dish_type", "cooks"]
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience",
+        )
+
+
+class CookLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["years_of_experience"]
+
+
+class CookSearchForm(forms.Form):
+    search = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"})
+    )
+
+
+class DishSearchForm(forms.Form):
+    search = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
+
+
+class DishTypeSearchForm(forms.Form):
+    search = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by type"})
+    )
--- dishes/views.py
+++ dishes/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views import generic
+
 from .models import Dish, Cook, DishType
+from .forms import DishForm, CookCreationForm, CookLicenseUpdateForm, CookSearchForm
 
 
 @login_required
@@ -20,3 +25,114 @@ def index(request):
     }
 
     return render(request, "dishes/index.html", context=context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "dishes/dish_type_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_query"] = self.request.GET.get("search_query", "")
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get("search_query", "")
+        if query:
+            return DishType.objects.filter(name__icontains=query)
+        return DishType.objects.all().order_by("id")
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("dishes:dish-type-list")
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("dishes:dish-type-list")
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("dishes:dish-type-list")
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    paginate_by = 5
+    queryset = Dish.objects.select_related("dish_type")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_query"] = self.request.GET.get("search_query", "")
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get("search_query", "")
+        if query:
+            return Dish.objects.filter(name__icontains=query)
+        return Dish.objects.all()
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("dishes:dish-list")
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("dishes:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("dishes:dish-list")
+
+
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = CookSearchForm(self.request.GET or None)
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get("search", "")
+        if query:
+            return Cook.objects.filter(username__icontains=query)
+        return Cook.objects.all()
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.all().prefetch_related("dishes__dish_type")
+
+
+class CookCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Cook
+    form_class = CookCreationForm
+    success_url = reverse_lazy("dishes:cook-list")
+
+
+class CookLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Cook
+    form_class = CookLicenseUpdateForm
+    success_url = reverse_lazy("dishes:cook-list")
+
+
+class CookDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Cook
+    success_url = reverse_lazy("dishes:cook-list")
