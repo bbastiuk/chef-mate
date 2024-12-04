6d18fa6ba43ec24c489f8ceb527ce3c0dc991947
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-12-04 15:47:00 +0100

    Add templates for dish type forms and delete confirmation

--- dishes/views.py
+++ dishes/views.py
@@ -10,14 +10,26 @@ from .forms import DishForm, CookCreationForm, CookLicenseUpdateForm, CookSearch
 
 @login_required
 def index(request):
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    dishes = Dish.objects.all()[:5]
+    dish_types = DishType.objects.all()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
-        'num_dishes': Dish.objects.count(),
-        'num_cooks': Cook.objects.count(),
-        'num_dish_types': DishType.objects.count(),
-        'num_visits': request.session.get('num_visits', 0),
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+        "dishes": dishes,
+        "dish_types": dish_types,
     }
-    request.session['num_visits'] = context['num_visits'] + 1
-    return render(request, 'dishes/index.html', context)
+
+    return render(request, "dishes/index.html", context=context)
 
 
 class DishTypeListView(LoginRequiredMixin, generic.ListView):
