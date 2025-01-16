fd41fe0a4ec625766d6e3d5ed5b1c3e319a64332
Bohdan Bastiuk <bbastyuk@gmail.com>
2025-01-16 19:35:27 +0100

    fix: update production settings with ALLOWED_HOSTS and database configuration

--- chef_mate/settings/prod.py
+++ chef_mate/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chef-mate.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
